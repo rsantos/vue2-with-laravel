125f9abbaea2a81488c31b0e4940338a61fdbe4f
Rafael Santos <rsanthos@gmail.com>
2017-06-15 14:48:05 -0300

    creating edit and create component

---
+++ src/components/Accounts/Create.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'accounts-create',
+  data: () => {
+    return {
+      sub_title: 'Criando conta'
+    }
+  },
+  template: require('./form.html')
+}
+</script>
---
+++ src/components/Accounts/Edit.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'accounts-edit',
+  data: () => {
+    return {
+      sub_title: 'Editando conta'
+    }
+  },
+  template: require('./form.html')
+}
+</script>
