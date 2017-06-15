113d78a298e90a86eb525cd5cb5d6c8a5a0cdd9d
Rafael Santos <rsanthos@gmail.com>
2017-06-15 13:59:52 -0300

    creating list component

---
+++ src/components/Accounts/List.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <div class="row">
+        <div class="content-header">
+            <h1>Contas bancárias <small>Gerenciamento de contas</small></h1>
+            <div class="grey-text">
+            <small>
+                <a href="#/">iFinance</a> >
+                <span>Suas contas</span>
+            </small>
+            </div>
+        </div>
+        <div class="card col s12">
+            <div class="card-content">
+                <table class="hi]ghlight">
+                    <thead>
+                    <tr>
+                        <th>contas</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr @click="goTo(1)">
+                        <td class="valign-wrapper">
+                          Conta título&nbsp;
+                          <small>agência: 0000 / conta: 0000  / código do banco: 000 </small>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-action">
+                <a href="#/contas/novo">Nova conta</a>
+            </div>
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+  name: 'accounts',
+  methods: {
+    goTo: function (id) {
+      this.$router.push('/contas/' + id)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  tbody tr {
+    cursor: pointer;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 import Hello from '@/components/Hello'
+import AccountsList from '@/components/Accounts/List'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Hello',
-    component: Hello
-  }
+  {path: '/', name: 'Hello', component: Hello},
+  {path: '/contas', component: AccountsList}
 ]
 
 export default routes
