ccbc6553a45847aeceaed35289e6612b7a05fccb
Rafael Santos <rsanthos@gmail.com>
2017-06-15 14:09:43 -0300

    creating view component

---
+++ src/components/Accounts/View.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div class="row">
+    <div class="content-header">
+      <h1>Conta título <small>Detalhes</small></h1>
+      <div class="grey-text">
+        <small>
+          <a href="#/">iFinance</a> >
+          <a href="#/contas">Suas contas</a> >
+          <span>Suas contas</span>
+        </small>
+      </div>
+    </div>
+    <div class="card col s12">
+      <div class="card-content">
+        <div class="row">
+          <div class="col s9">
+            <div class="card-title">Detalhes da conta</div>
+            <p>Agência: 0000 / Conta: 0000 </p>
+            <p>Banco: Banco título - 000</p>
+            <p>Valor inicial da conta: R$ 0,00</p>
+          </div>
+          <div class="col s3 right-align">
+            <p>Saldo: <span class="btn disabled">R$ 0,00</span></p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12">
+            <a :href="'#/contas/2/editar'" class="btn blue">Editar</a>
+            <a href="" class="btn red" @click.prevent="remove(1)" >Excluir</a>
+          </div>
+        </div>
+      </div>
+      <div class="card-action">
+        <a href="#/contas">Voltar</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'accounts-view',
+  methods: {
+    remove: function (id) {
+      this.$router.push('/contas')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Hello from '@/components/Hello'
 import AccountsList from '@/components/Accounts/List'
+import AccountsView from '@/components/Accounts/View'
 
 const routes = [
   {path: '/', name: 'Hello', component: Hello},
-  {path: '/contas', component: AccountsList}
+  {path: '/contas', component: AccountsList},
+  {path: '/contas/:id', component: AccountsView}
 ]
 
 export default routes
